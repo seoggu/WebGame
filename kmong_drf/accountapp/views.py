7473d8531710a2db6170a3ed55e78d788ba90ce2
seoggu <pshn123@naver.com>
2023-11-23 12:37:16 +0900

    jwtviews

--- kmong_drf/accountapp/views.py
+++ kmong_drf/accountapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+
+class CustomTokenRefreshView(TokenRefreshView):
+    permission_classes = (permissions.AllowAny,)
+
+class CustomTokenVerifyView(TokenVerifyView):
+    permission_classes = (permissions.AllowAny,)
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response(data={"message": "Hello, World!"}, status=status.HTTP_200_OK)
